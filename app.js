f3ad2df5baba339740066ce6a70d3900edb990dd
Aleksanyaan <alexanyan.jenya@gmail.com>
2023-06-03 13:28:06 +0400

    Firs homework about files

--- app.js
+++ app.js
@@ -3,82 +3,18 @@ import os from 'os';
 import fs from 'node:fs';
 import path from 'path';
 
-const currentDir = path.dirname(new URL(import.meta.url).pathname);
+const currentDir = process.cwd();
 
 process.stdin.setEncoding('utf8');
 process.stdout.write(`Welcome ${os.userInfo().username} \n`);
 
 process.stdin.on('data',(data) => {
     const input = data.trim();
-    const fileName = input.slice(4).trim();
+    
 
     const parts = input.split(' ');
     const firstOp = parts[1];
     const secondOp = parts[2];
-
-    if (input.startsWith('add') && fileName.length > 0) {
-        fs.writeFile(fileName, '', (err) => {
-            if (err) throw err;
-            console.log('File is created successfully.');
-        });
-    }
-    if (input.startsWith('rn') && firstOp.length > 0 && secondOp.length > 0) {
-        fs.rename(firstOp, secondOp, function(err) {
-            if (err) throw err;
-            console.log('File Renamed.');
-        });
-    }
-
-    if (input.startsWith('cp') && firstOp.length > 0 && secondOp.length > 0) {
-        const destinationPath = `${secondOp}/${firstOp}`;
-
-        fs.copyFile(firstOp, destinationPath, (err) => {
-            if (err) throw err;
-            console.log(`${firstOp} was copied to ${destinationPath}`);
-        });
-    }
-
-    
-    // if (input.startsWith('mv') && firstOp.length > 0 && secondOp.length > 0) {
-    //     const currentPath = path.join(currentDir, firstOp);
-    //     const newPath = path.join(currentDir,firstOp, secondOp);
-
-    //     if (!fs.existsSync(currentPath)) {
-    //         console.error("Error: Source file does not exist.");
-    //         return;
-    //     }
-
-    //     const destinationDir = path.dirname(newPath);
-    //     if (!fs.existsSync(destinationDir)) {
-    //         try {
-    //             fs.mkdirSync(destinationDir, { recursive: true });
-    //         } catch (err) {
-    //             console.error("Error occurred while creating the destination directory:", err);
-    //             return;
-    //         }
-    //     }
-
-    //     fs.rename(currentPath, newPath, (err) => {
-    //         if (err) {
-    //             console.error("Error occurred while moving the file:", err);
-    //         } else {
-    //             console.log("Successfully moved the file!");
-    //         }
-    //     });
-    // }
-    
-
-    if(input.startsWith('rm') && firstOp.length > 0) {
-        fs.unlink(firstOp, (err) => {
-            if (err) {
-              console.error("Error occurred while deleting the file:", err);
-            } else {
-              console.log("File deleted successfully");
-            }
-        });
-    }
-
-    
     
     switch (input) {
         case '.exit':
@@ -116,6 +52,76 @@ process.stdin.on('data',(data) => {
             process.stdout.write('Invalid input \n');
             process.stdin.read();
             break;
+    }
+
+    if(input === 'ls') {
+        fs.readdir(currentDir, { withFileTypes: true }, (err, files) => {
+            if (err) {
+                console.error("Error occurred while reading directory:", err);
+            }
+            const folders = [];
+            const filesList = [];
+
+            files.forEach((file) => {
+                if (file.isDirectory()) {
+                    folders.push(file.name);
+                } else {
+                    filesList.push(file.name);
+                }
+            });
+
+            let index = 0;
+            folders.sort();
+            filesList.sort();
+
+            folders.forEach((folder, i) => {
+                console.log(`${i}   |    ${folder}   |    'dicrectory'`);
+                index = i;
+            });
+
+            filesList.forEach((file) => {
+                const extension = path.extname(file);
+                console.log(`${++index}  |   ${file} (${extension})   |    'file'`);
+            });
+        });
+    } else if (input.startsWith('add') && firstOp.length > 0) {
+        fs.writeFile(firstOp, '', (err) => {
+            if (err) throw err;
+            console.log('File is created successfully.');
+        });
+    } else if (input.startsWith('rn') && firstOp.length > 0 && secondOp.length > 0) {
+        fs.rename(firstOp, secondOp, function(err) {
+            if (err) throw err;
+            console.log('File Renamed.');
+        });
+    } else if (input.startsWith('cp') && firstOp.length > 0 && secondOp.length > 0) {
+        const destinationPath = `${secondOp}/${firstOp}`;
+
+        fs.copyFile(firstOp, destinationPath, (err) => {
+            if (err) throw err;
+            console.log(`${firstOp} was copied to ${destinationPath}`);
+        });
+    } else if (input.startsWith('mv') && firstOp.length > 0 && secondOp.length > 0) {
+        const currentPath = path.join(currentDir, firstOp);
+        const newPath = path.join(currentDir, secondOp, firstOp);
+
+        fs.rename(currentPath, newPath, (err) => {
+            if (err) {
+                console.error("Error occurred while moving the file:", err);
+            } else {
+                console.log("Successfully moved the file!");
+            }
+        });
+    } else if(input.startsWith('rm') && firstOp.length > 0) {
+        fs.unlink(firstOp, (err) => {
+            if (err) {
+              console.error("Error occurred while deleting the file:", err);
+            } else {
+              console.log("File deleted successfully");
+            }
+        });
+    } else {
+        console.log('Invalid input');
     }
         
 });
